eab800f5b10c0bea3238f12833f2c4981026872f
root <root@DESKTOP-AR929QJ.localdomain>
2017-12-27 12:48:32 +0800

    Update Test Code 2017/12/27 - 12:48

--- database_tester.py
+++ database_tester.py
@@ -1,13 +1,14 @@
 import pymysql
-db = pymysql.connect("127.0.0.1","root","0000","test_database")
+db = pymysql.connect("127.0.0.1", "root", "0000", "test_database")
 cursor = db.cursor()
 
 sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
          LAST_NAME, AGE)
-         VALUES ('Mac', 'Mohan', 20)"""
+         VALUES ('aaa', 'bbb', 20)"""
 try:
    cursor.execute(sql)
    db.commit()
 except:
    db.rollback()
 db.close()
+
